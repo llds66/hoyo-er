96241bb28ba58f139f5b37fb25010ba95df0ed2c
llds <858400061@qq.com>
2025-07-08 20:16:05 +0800

    ✨ feat:跳转链接

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -29,10 +29,12 @@ const gameData = ref()
 async function getGameData(gameId: string, keyword: string) {
   const res = await fetch(`/json/${gameId}.json`)
   const json = await res.json()
+
   gameData.value = keyword
-    ? json.filter((item: any) =>
-        item.keywords?.includes(keyword),
-      )
+    ? json.filter((item: any) => {
+        const keywords = item.keywords ? JSON.parse(item.keywords) : []
+        return keywords.includes(keyword)
+      })
     : json
 }
 </script>
@@ -41,7 +43,7 @@ async function getGameData(gameId: string, keyword: string) {
   <div class="box">
     <Search class="py-3 top-0 sticky z-10" @search="handleSearch" @empty="emptyEvent" />
     <div v-if="gameData?.length" class="biliData_grid">
-      <BiliData v-for="i in gameData" :key="i.id" :bvid="i.bvid" :video-data="i" />
+      <BiliData v-for="i in gameData" :key="i.bvid" :video-data="i" />
     </div>
     <div v-else class="text-sm text-stone-600 mt-40 flex-center gap-2 w-full">
       <div class="i-iconamoon-information-circle-duotone" />
